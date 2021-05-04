09d3daa4e000daabc30e039d528a50584b6d2e42
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-05-03 20:35:07 -0500

    Compare AVX-enabled flip vs not

--- aten/src/ATen/native/TensorTransformations.cpp
+++ aten/src/ATen/native/TensorTransformations.cpp
@@ -136,6 +136,48 @@ static TensorIterator make_index_iterator(const Tensor input, const std::vector<
   return config.build();
 }
 
+struct Indexer {
+  Indexer(int64_t num_indexers, char** indexers, const int64_t* indexer_strides)
+      : num_indexers(num_indexers),
+        indexers(indexers),
+        indexer_strides(indexer_strides) {}
+
+  int64_t num_indexers;
+  char** indexers;
+  const int64_t* indexer_strides;
+
+  int64_t get(int64_t idx) {
+    int64_t offset = *(int64_t*)&indexers[0][idx * indexer_strides[0]];
+    for (int j = 1; j < num_indexers; j++) {
+      offset += *(int64_t*)&indexers[j][idx * indexer_strides[j]];
+    }
+    return offset;
+  }
+};
+
+template <typename scalar_t>
+void flip_cpu_kernel(TensorIterator& iter) {
+  int ntensor = iter.ntensors();
+  // When launch the index parallel version, set a relative samll grain size
+  // less than the INTERNAL::GRAIN_SIZE to make the whole available thread
+  // numbers get more balanced work load and a better cache location. The grain
+  // size here is chosen by the op benchmark to overcome the thread launch
+  // overhead This value was taken from the AdvancedIndexing kernel.
+  const int index_parallel_grain_size = 3000;
+  auto loop = [&](char** data, const int64_t* strides, int64_t n) {
+    auto indexer = Indexer(ntensor - 2, &data[2], &strides[2]);
+    char* dst = data[0];
+    char* src = data[1];
+
+    for (int64_t i = 0; i < n; i++) {
+      int64_t offset = indexer.get(i);
+      *(scalar_t*)(dst + strides[0] * i) =
+          *(scalar_t*)(src + strides[1] * i + offset);
+    }
+  };
+
+  iter.for_each(loop, index_parallel_grain_size);
+}
 
 Tensor flip_cpu(const Tensor& self, IntArrayRef dims) {
   auto input = self;
@@ -163,6 +205,47 @@ Tensor flip_cpu(const Tensor& self, IntArrayRef dims) {
   auto iter = make_index_iterator(restrided_input, indices);
 
   flip_stub(iter.device_type(), iter, input);
+
+  auto result = iter.output();
+  return result;
+}
+
+Tensor flip_cpu_internal(const Tensor& self, IntArrayRef dims) {
+  auto input = self;
+  const int64_t total_dims = input.dim();
+  auto flip_dims_b = at::dim_list_to_bitset(dims, total_dims);
+
+  std::vector<int64_t> flip_dims;
+  for(int64_t i = 0; i < total_dims; i++) {
+      if(flip_dims_b[i]) {
+        flip_dims.push_back(i);
+      }
+  }
+
+  auto shape = input.sizes().vec();
+  auto strides = input.strides().vec();
+
+  // Set stride to zero on the dimensions that are going to be flipped
+  for(auto dim: flip_dims) {
+    strides[dim] = 0;
+  }
+
+  // Restride the input to index only on the dimensions to flip
+  auto restrided_input = input.as_strided(shape, strides);
+  auto indices = build_indices_loop(input, flip_dims);
+  auto iter = make_index_iterator(restrided_input, indices);
+
+  if (input.is_quantized()) {
+    AT_DISPATCH_QINT_AND_SUB_BYTE_TYPES(
+        input.scalar_type(), "flip_quantized_cpu", [&] {
+          flip_cpu_kernel<scalar_t>(iter);
+        });
+  } else {
+    AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(
+        kBool, kHalf, kBFloat16, input.scalar_type(), "flip_cpu", [&] {
+          flip_cpu_kernel<scalar_t>(iter);
+        });
+  }
 
   auto result = iter.output();
   return result;
